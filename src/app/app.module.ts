3f48b5d76fcad8e21a359372d37e1aa1dc41a795
Luis Henrique Ries <prof.ries@gmail.com>
2024-03-25 23:55:47 -0300

    Inicio do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabelaProdutosComponent } from './tabela-produtos/tabela-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaProdutosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tabela-produtos/tabela-produtos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-produtos',
+  templateUrl: './tabela-produtos.component.html',
+  styleUrl: './tabela-produtos.component.css'
+})
+export class TabelaProdutosComponent {
+
+}
